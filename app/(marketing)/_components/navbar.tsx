5c1d1af8e8c123f11b1955279bd29b057fda5998
VictorDiasO <victordiasdeveloper@gmail.com>
2024-01-30 19:27:40 -0300

    Improving navbar and creating the spinner component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,9 +5,11 @@ import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
-  const {} = useConvexAuth();
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -21,6 +23,26 @@ export const Navbar = () => {
       <div
         className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2"
       >
+        {isLoading && (
+          <p>
+            Loading...
+          </p>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">
+                Get Votion Free
+              </Button>
+            </SignInButton>
+          </>
+        )}
+
         <ModeToggle />
       </div>
     </div>
