b16534695116ba86df7a7931355e93007e374caa
VictorDiasO <victordiasdeveloper@gmail.com>
2024-02-01 12:07:54 -0300

    User item menu

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -19,6 +24,13 @@ const DocumentsPage = () => {
         alt="Empty image, a person opening a empty book"
         className="hidden dark:block"
       />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Votion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
     </div>
   );
 }
