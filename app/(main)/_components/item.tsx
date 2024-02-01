40898cde91b6b81dbae786debde4b22142b14742
VictorDiasO <victordiasdeveloper@gmail.com>
2024-02-01 18:37:19 -0300

    Finalizing the Document Item to our Navbar

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
@@ -35,11 +36,11 @@ export const Item = ({
     <button
       onClick={onClick}
       style={{
-        paddingLeft: level ? `${(level * 12) + 12}px` : "12px"
+        paddingLeft: level ? `${level * 12 + 12}px` : "12px",
       }}
       className={cn(
         "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
-        active && "bg-primary/5 text-primary"
+        active && "bg-primary/5 text-primary",
       )}
     >
       {!!id && (
@@ -47,29 +48,34 @@ export const Item = ({
           className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
           onClick={() => {}}
         >
-          <ChevronIcon
-            className="h-4 w-4 shrink-0 text-muted-foreground/50"
-          />
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </button>
       )}
-      { documentIcon
-        ? (
-          <div className="shrink-0 mr-2 text-[18px]">
-            {documentIcon}
-          </div>
-        )
-        : (
-          <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
-        )
-      }
+      {documentIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
       <span className="truncate">{label}</span>
       {isSearch && (
-        <kbd
-          className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100"
-        >
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">ctrl</span>+ K
         </kbd>
       )}
     </button>
+  );
+};
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${level * 12 + 25}px` : "12px",
+      }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
+    </div>
   );
 };
