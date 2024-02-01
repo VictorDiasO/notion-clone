c9f814827d9382b99b91556c488794cfd4b59653
VictorDiasO <victordiasdeveloper@gmail.com>
2024-02-01 11:08:11 -0300

    Setting up the documents page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import Image from "next/image";
+
 const DocumentsPage = () => {
   return (
-    <div>
-      This is a protected document page
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty image, a person opening a empty book"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty image, a person opening a empty book"
+        className="hidden dark:block"
+      />
     </div>
   );
 }
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -14,15 +14,11 @@ export const Navigation = () => {
   const sidebarRef = useRef<ElementRef<"aside">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null);
 
-  const [ isResetting, setIsResetting ] = useState(false);
-  const [ isCollapsed, setIsCollapsed ] = useState(isMobile);
-
-  useEffect(() => {
-    setIsCollapsed(isMobile)
-  }, [isMobile])
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
   const handleMouseDown = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
   ) => {
     event.preventDefault();
     event.stopPropagation();
@@ -42,7 +38,10 @@ export const Navigation = () => {
     if (sidebarRef.current && navbarRef.current) {
       sidebarRef.current.style.width = `${newWidth}px`;
       navbarRef.current.style.setProperty("left", `${newWidth}px`);
-      navbarRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`);
+      navbarRef.current.style.setProperty(
+        "width",
+        `calc(100% - ${newWidth}px)`,
+      );
     }
   };
 
@@ -60,12 +59,9 @@ export const Navigation = () => {
       sidebarRef.current.style.width = isMobile ? "100%" : "240px";
       navbarRef.current.style.setProperty(
         "width",
-        isMobile ? "0" : "calc(100% - 240px)"
+        isMobile ? "0" : "calc(100% - 240px)",
       );
-      navbarRef.current.style.setProperty(
-        "left",
-        isMobile ? "100%" : "240px"
-      );
+      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
       setTimeout(() => setIsResetting(false), 300);
     }
   };
@@ -81,7 +77,21 @@ export const Navigation = () => {
 
       setTimeout(() => setIsResetting(false), 300);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    }
+  }, [pathname, isMobile]);
 
   return (
     <>
@@ -89,14 +99,14 @@ export const Navigation = () => {
         ref={sidebarRef}
         className={cn(
           "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
-          isResetting &&"transition-all ease-in-out duration-300",
-          isMobile && "w-0"
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0",
         )}
       >
         <button
           className={cn(
             "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
-            isMobile && "opacity-100"
+            isMobile && "opacity-100",
           )}
           onClick={collapse}
         >
@@ -119,19 +129,19 @@ export const Navigation = () => {
         className={cn(
           "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
           isResetting && "transition-all ease-in-out duration-300",
-          isMobile && "left-0 w-full"
+          isMobile && "left-0 w-full",
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
-          { isCollapsed &&
+          {isCollapsed && (
             <MenuIcon
               role="button"
               className="h-6 w-6 text-muted-foreground"
               onClick={resetWidth}
             />
-          }
+          )}
         </nav>
       </div>
     </>
   );
-}
+};
