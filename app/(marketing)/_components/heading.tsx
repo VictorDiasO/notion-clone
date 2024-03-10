c7805ea99b5e291cc14d776f2ef0385510d0fc45
VictorDiasO <victordiasdeveloper@gmail.com>
2024-03-10 15:59:54 -0300

    Changing the marketing page

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -13,7 +13,8 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your ideas, Documents & Plans. Unified. Welcome to <span className="underline">Votion</span>
+        Your ideas, Documents & Plans Unified. Welcome to{" "}
+        <span className="underline">Votion</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Votion is the place that you can work <br />
@@ -42,4 +43,4 @@ export const Heading = () => {
       )}
     </div>
   );
-}
+};
