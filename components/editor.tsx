64c0c1f533342ee18b5449e6d8ac4492eee8416e
VictorDiasO <victordiasdeveloper@gmail.com>
2024-03-10 07:18:35 -0300

    Optimizing the Editor component

--- components/editor.tsx
+++ components/editor.tsx
@@ -14,7 +14,7 @@ interface EditorProps {
   editable?: boolean;
 }
 
-export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
   const { edgestore } = useEdgeStore();
 
@@ -47,3 +47,5 @@ export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
     </div>
   );
 };
+
+export default Editor;
