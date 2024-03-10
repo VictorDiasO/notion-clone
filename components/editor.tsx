f7ece972251c57e98308829a65e1ca920faadb40
VictorDiasO <victordiasdeveloper@gmail.com>
2024-03-10 06:53:37 -0300

    Adding Image upload functionality to our editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -6,6 +6,7 @@ import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
 import { useEffect } from "react";
 import { useTheme } from "next-themes";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface EditorProps {
   onChange: (value: string) => void;
@@ -15,11 +16,21 @@ interface EditorProps {
 
 export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+
+    return response.url;
+  };
 
   const editor = useCreateBlockNote({
     initialContent: initialContent
       ? (JSON.parse(initialContent) as PartialBlock[])
       : undefined,
+    uploadFile: handleUpload,
   });
 
   editor.onEditorContentChange(() => {
